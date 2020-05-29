9ba05f4870451793d7f2103418004a1201466ac2
Matt March <matthewmarch29@gmail.com>
2020-05-29 12:54:18 +0100

    Add datepicker, chart data fetcher from jsonbin and logic to choose birthday number ones

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import {
+  getChartData,
+  ChartData,
+  findBirthdayNumberOnes,
+  BirthdayNumberOnes,
+} from "./ChartData";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
+  const [birthdayDate, setBirthdayDate] = useState(new Date());
+  const [chartData, setChartData] = useState<ChartData | null>(null);
+  const [
+    birthdayNumberOnes,
+    setBirthdayNumberOnes,
+  ] = useState<BirthdayNumberOnes | null>(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getChartData();
+      setChartData(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        Generate a Spotify playlist of UK number ones on your Birthday since you
+        were born.
+      </p>
+
+      <h3>Enter your birthday below:</h3>
+      <DatePicker
+        selected={birthdayDate}
+        onChange={(date) =>
+          date ? setBirthdayDate(date) : setBirthdayDate(new Date())
+        }
+        dateFormat="dd/MM/yyyy"
+        minDate={new Date(1900, 0, 1)}
+        maxDate={new Date()}
+        showMonthDropdown
+        showYearDropdown
+        dropdownMode="select"
+      />
+      <button
+        disabled={chartData == null}
+        onClick={() =>
+          chartData &&
+          setBirthdayNumberOnes(findBirthdayNumberOnes(birthdayDate, chartData))
+        }
+      >
+        Find me a playlist
+      </button>
     </div>
   );
 }
---
+++ app/src/ChartData.ts
@@ -0,0 +1,64 @@
+import { findLast } from "lodash";
+import { DateTime, Interval } from "luxon";
+
+const JsonBinUrl = "https://api.jsonbin.io/b/5ecfe1657741ef56a5638007";
+
+type IncomingChartEntry = {
+  start_date: string;
+  title: string;
+  artist: string;
+};
+
+export type ChartEntry = {
+  startDate: DateTime;
+  title: string;
+  artist: string;
+};
+
+export type ChartData = Array<ChartEntry>;
+
+export const getChartData: () => Promise<ChartData> = () =>
+  fetch(JsonBinUrl)
+    .then((response) => response.json())
+    .then((incomingEntries) =>
+      incomingEntries.map((entry: IncomingChartEntry) => ({
+        startDate: DateTime.fromISO(entry.start_date),
+        title: entry.title,
+        artist: entry.artist,
+      }))
+    );
+
+export type BirthdayNumberOnes = Array<Birthday>;
+
+export type Birthday = {
+  date: DateTime;
+  numberOne: ChartEntry | NoData;
+};
+
+enum NoData {
+  DatePreCharts,
+  LackingCurrentData,
+}
+
+export const findBirthdayNumberOnes = (
+  birthdayDate: Date,
+  chartData: ChartData
+): BirthdayNumberOnes =>
+  Interval.fromDateTimes(DateTime.fromJSDate(birthdayDate), DateTime.local())
+    .splitBy({ years: 1 })
+    .map((interval) => findBirthdayNumberOne(interval.start, chartData));
+
+const findBirthdayNumberOne = (
+  birthday: DateTime,
+  chartData: ChartData
+): Birthday => {
+  const chartEntryBeforeBirthday = findLast(
+    chartData,
+    (entry) => entry.startDate <= birthday
+  );
+  if (chartEntryBeforeBirthday === undefined) {
+    return { date: birthday, numberOne: NoData.DatePreCharts };
+  } else {
+    return { date: birthday, numberOne: chartEntryBeforeBirthday };
+  }
+};
