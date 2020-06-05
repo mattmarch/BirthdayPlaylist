aa27187217c4de36a33832bdd440464612ffd791
Matt March <matthewmarch29@gmail.com>
2020-06-05 10:15:04 +0100

    Add ReactRouter

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,111 +1,17 @@
-import React, { useState, useEffect } from "react";
-// import "./App.css";
-import {
-  getChartData,
-  ChartData,
-  findBirthdayNumberOnes,
-  BirthdayNumberOnes,
-  NoDataReason,
-} from "./ChartData";
-import DatePicker from "react-datepicker";
+import React from "react";
 import "react-datepicker/dist/react-datepicker.css";
-import styled from "styled-components";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-const App = () => {
-  const [birthdayDate, setBirthdayDate] = useState(new Date());
-  const [chartData, setChartData] = useState<ChartData | null>(null);
-  const [
-    birthdayNumberOnes,
-    setBirthdayNumberOnes,
-  ] = useState<BirthdayNumberOnes | null>(null);
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getChartData();
-      setChartData(data);
-    };
-    fetchData();
-  }, []);
-
-  return (
-    <AppContainer>
-      <h1>Birthday Playlist Generator</h1>
-      <p>
-        Generate a Spotify playlist of UK number ones on your Birthday since you
-        were born. This site is a work in progress.
-      </p>
-
-      <h3>Enter your birthday below:</h3>
-      <DatePicker
-        selected={birthdayDate}
-        onChange={(date) =>
-          date ? setBirthdayDate(date) : setBirthdayDate(new Date())
-        }
-        dateFormat="dd/MM/yyyy"
-        minDate={new Date(1900, 0, 1)}
-        maxDate={new Date()}
-        showMonthDropdown
-        showYearDropdown
-        dropdownMode="select"
-      />
-      <SubmitButton
-        disabled={chartData == null}
-        onClick={() =>
-          chartData &&
-          setBirthdayNumberOnes(findBirthdayNumberOnes(birthdayDate, chartData))
-        }
-      >
-        Find me a playlist
-      </SubmitButton>
-      {birthdayNumberOnes && (
-        <ResultsContainer>
-          <a href="https://accounts.spotify.com/authorize?client_id=6c0a042391fa42e8ac96a5eed4306dfe&redirect_uri=http:%2F%2Flocalhost:3000&scope=playlist-modify-public&response_type=token&state=123">
-            Connect with Spotify for more track information and the option to automatically create a playlist.
-          </a>
-          <NumberOnesList birthdayNumberOnes={birthdayNumberOnes} />
-        </ResultsContainer>
-      )}
-    </AppContainer>
-  );
-};
-
-const NumberOnesList = (props: { birthdayNumberOnes: BirthdayNumberOnes }) => (
-  <div>
-    {props.birthdayNumberOnes.map((birthdayEntry) => (
-      <Result key={birthdayEntry.date.toLocaleString()}>
-        <h4>{birthdayEntry.date.toLocaleString()}</h4>
-        {birthdayEntry.numberOne ? (
-          <p>
-            {birthdayEntry.numberOne.title} by {birthdayEntry.numberOne.artist}
-          </p>
-        ) : birthdayEntry.reason === NoDataReason.DATE_TOO_OLD ? (
-          <p>UK Charts only started on 08/11/1952</p>
-        ) : (
-          <p>The latest chart data hasn't been updated yet, try again soon!</p>
-        )}
-      </Result>
-    ))}
-  </div>
+const App = () => (
+  <Router>
+    <Switch>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </Router>
 );
 
-const AppContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 20px;
-`;
-
-const SubmitButton = styled.button`
-  margin: 10px;
-`;
-
-const ResultsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const Result = styled.div`
-  text-align: center;
-`;
-
 export default App;
+
---
+++ app/src/Home.tsx
@@ -0,0 +1,110 @@
+import React, { useState, useEffect } from "react";
+import {
+  getChartData,
+  ChartData,
+  findBirthdayNumberOnes,
+  BirthdayNumberOnes,
+  NoDataReason,
+} from "./ChartData";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import styled from "styled-components";
+
+const Home = () => {
+  const [birthdayDate, setBirthdayDate] = useState(new Date());
+  const [chartData, setChartData] = useState<ChartData | null>(null);
+  const [
+    birthdayNumberOnes,
+    setBirthdayNumberOnes,
+  ] = useState<BirthdayNumberOnes | null>(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getChartData();
+      setChartData(data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <AppContainer>
+      <h1>Birthday Playlist Generator</h1>
+      <p>
+        Generate a Spotify playlist of UK number ones on your Birthday since you
+        were born. This site is a work in progress.
+      </p>
+
+      <h3>Enter your birthday below:</h3>
+      <DatePicker
+        selected={birthdayDate}
+        onChange={(date) =>
+          date ? setBirthdayDate(date) : setBirthdayDate(new Date())
+        }
+        dateFormat="dd/MM/yyyy"
+        minDate={new Date(1900, 0, 1)}
+        maxDate={new Date()}
+        showMonthDropdown
+        showYearDropdown
+        dropdownMode="select"
+      />
+      <SubmitButton
+        disabled={chartData == null}
+        onClick={() =>
+          chartData &&
+          setBirthdayNumberOnes(findBirthdayNumberOnes(birthdayDate, chartData))
+        }
+      >
+        Find me a playlist
+      </SubmitButton>
+      {birthdayNumberOnes && (
+        <ResultsContainer>
+          <a href="https://accounts.spotify.com/authorize?client_id=6c0a042391fa42e8ac96a5eed4306dfe&redirect_uri=http:%2F%2Flocalhost:3000%2Fspotify-callback&scope=playlist-modify-public&response_type=token&state=123">
+            Connect with Spotify for more track information and the option to automatically create a playlist.
+          </a>
+          <NumberOnesList birthdayNumberOnes={birthdayNumberOnes} />
+        </ResultsContainer>
+      )}
+    </AppContainer>
+  );
+};
+
+const NumberOnesList = (props: { birthdayNumberOnes: BirthdayNumberOnes }) => (
+  <div>
+    {props.birthdayNumberOnes.map((birthdayEntry) => (
+      <Result key={birthdayEntry.date.toLocaleString()}>
+        <h4>{birthdayEntry.date.toLocaleString()}</h4>
+        {birthdayEntry.numberOne ? (
+          <p>
+            {birthdayEntry.numberOne.title} by {birthdayEntry.numberOne.artist}
+          </p>
+        ) : birthdayEntry.reason === NoDataReason.DATE_TOO_OLD ? (
+          <p>UK Charts only started on 08/11/1952</p>
+        ) : (
+          <p>The latest chart data hasn't been updated yet, try again soon!</p>
+        )}
+      </Result>
+    ))}
+  </div>
+);
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+`;
+
+const SubmitButton = styled.button`
+  margin: 10px;
+`;
+
+const ResultsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Result = styled.div`
+  text-align: center;
+`;
+
+export default Home;
