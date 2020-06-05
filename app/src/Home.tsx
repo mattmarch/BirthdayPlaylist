27912ef0a431987fae34f207667c1eac517653a1
Matt March <matthewmarch29@gmail.com>
2020-06-05 10:32:58 +0100

    Move BirthdayPicker into it's own component/file

--- app/src/Home.tsx
+++ app/src/Home.tsx
@@ -1,17 +1,16 @@
-import React, { useState, useEffect } from "react";
-import {
-  getChartData,
-  ChartData,
-  findBirthdayNumberOnes,
-  BirthdayNumberOnes,
-  NoDataReason,
-} from "./ChartData";
-import DatePicker from "react-datepicker";
+import React, { useEffect, useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
 import styled from "styled-components";
+import {
+  BirthdayNumberOnes,
+  ChartData,
+  findBirthdayNumberOnes,
+  getChartData,
+  NoDataReason,
+} from "./ChartData";
+import BirthdayPicker from "./shared/BirthdayPicker";
 
 const Home = () => {
-  const [birthdayDate, setBirthdayDate] = useState(new Date());
   const [chartData, setChartData] = useState<ChartData | null>(null);
   const [
     birthdayNumberOnes,
@@ -32,33 +31,18 @@ const Home = () => {
         Generate a Spotify playlist of UK number ones on your Birthday since you
         were born. This site is a work in progress.
       </p>
-
-      <h3>Enter your birthday below:</h3>
-      <DatePicker
-        selected={birthdayDate}
-        onChange={(date) =>
-          date ? setBirthdayDate(date) : setBirthdayDate(new Date())
-        }
-        dateFormat="dd/MM/yyyy"
-        minDate={new Date(1900, 0, 1)}
-        maxDate={new Date()}
-        showMonthDropdown
-        showYearDropdown
-        dropdownMode="select"
-      />
-      <SubmitButton
+      <BirthdayPicker
         disabled={chartData == null}
-        onClick={() =>
-          chartData &&
-          setBirthdayNumberOnes(findBirthdayNumberOnes(birthdayDate, chartData))
+        onDateSelect={(date) =>
+          chartData != null &&
+          setBirthdayNumberOnes(findBirthdayNumberOnes(date, chartData))
         }
-      >
-        Find me a playlist
-      </SubmitButton>
+      />
       {birthdayNumberOnes && (
         <ResultsContainer>
           <a href="https://accounts.spotify.com/authorize?client_id=6c0a042391fa42e8ac96a5eed4306dfe&redirect_uri=http:%2F%2Flocalhost:3000%2Fspotify-callback&scope=playlist-modify-public&response_type=token&state=123">
-            Connect with Spotify for more track information and the option to automatically create a playlist.
+            Connect with Spotify for more track information and the option to
+            automatically create a playlist.
           </a>
           <NumberOnesList birthdayNumberOnes={birthdayNumberOnes} />
         </ResultsContainer>
@@ -91,10 +75,6 @@ const AppContainer = styled.div`
   flex-direction: column;
   align-items: center;
   padding: 20px;
-`;
-
-const SubmitButton = styled.button`
-  margin: 10px;
 `;
 
 const ResultsContainer = styled.div`
---
+++ app/src/shared/BirthdayPicker.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import DatePicker from "react-datepicker";
+
+type Props = {
+  disabled: boolean;
+  onDateSelect: (date: Date) => void;
+};
+
+const BirthdayPicker = (props: Props) => {
+  const [birthdayDate, setBirthdayDate] = useState(new Date());
+
+  return (
+    <Container>
+      <h3>Enter your birthday below:</h3>
+      <DatePicker
+        selected={birthdayDate}
+        onChange={(date) =>
+          date ? setBirthdayDate(date) : setBirthdayDate(new Date())
+        }
+        dateFormat="dd/MM/yyyy"
+        minDate={new Date(1900, 0, 1)}
+        maxDate={new Date()}
+        showMonthDropdown
+        showYearDropdown
+        dropdownMode="select"
+      />
+      <SubmitButton
+        disabled={props.disabled}
+        onClick={() => props.onDateSelect(birthdayDate)}
+      >
+        Find me a playlist
+      </SubmitButton>
+    </Container>
+  );
+};
+
+const SubmitButton = styled.button`
+  margin: 10px;
+`;
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+`
+
+export default BirthdayPicker;
