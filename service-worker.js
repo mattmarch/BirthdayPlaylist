346794c861afca72843ca0da265c5f5f53dabbf7
mattmarch <mattmarch@users.noreply.github.com>
2020-06-18 14:02:09 +0000

    Deploying to gh-pages from  @ f2bfeca700c5dbed8578c4a4cd3f84ddd7131ec2 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9620075a5a03da45f273c90e0e05cf7a.js"
+  "/precache-manifest.694b8f509aef16cf3d40c2b35c9344a6.js"
 );
 
 self.addEventListener('message', (event) => {
